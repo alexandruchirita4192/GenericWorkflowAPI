893aac99c6dd4f043751ec112bcb3f5744092e16
Alexandru Chirita <34884567+alexandruchirita4192@users.noreply.github.com>
2022-01-24 22:52:38 +0200

    Test all generic repository methods

--- src/GenericWorkflowAPI.UnitTesting/TestRepositories/TestGenericCodeRepository.cs
+++ src/GenericWorkflowAPI.UnitTesting/TestRepositories/TestGenericCodeRepository.cs
@@ -44,7 +44,7 @@ namespace GenericWorkflowAPI.UnitTesting
         }
 
         [TestMethod]
-        public async Task TestInsertGetAndDelete()
+        public async Task TestAddGetUpdateAndDelete()
         {
             ServicesExtensions.RegisterEncodingProvider();
 
@@ -55,16 +55,65 @@ namespace GenericWorkflowAPI.UnitTesting
             var code = $"Test{DateTime.Now}";
             var entity = new Workflow()
             {
-                Code = code
+                Code = code,
+                Description = string.Empty
             };
             await repository.AddAsync(entity, user, cancellationToken);
 
             entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
             Assert.IsNotNull(entity);
             Assert.AreEqual(code, entity.Code);
+            Assert.AreEqual(string.Empty, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            var description = $"Description{DateTime.Now}";
+
+            entity.Description = description;
+            await repository.UpdateAsync(entity, user, cancellationToken);
+
+            entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
+            Assert.AreEqual(description, entity.Description);
             Assert.IsFalse(entity.IsDeleted);
 
             await repository.DeleteAsync(code, user, cancellationToken);
+
+            entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
+            Assert.IsNull(entity);
+        }
+
+        [TestMethod]
+        public async Task TestAddRangeGetAndDelete()
+        {
+            ServicesExtensions.RegisterEncodingProvider();
+
+            var cancellationToken = new CancellationToken();
+            var repository = GetGenericCodeRepository<Workflow>(isInMemoryDbContext: true);
+
+            var user = new IdentityUser("admin") { Id = 1 };
+            var code = $"Test{DateTime.Now}";
+            var entity = new Workflow()
+            {
+                Code = code,
+                Description = string.Empty
+            };
+            await repository.AddRangeAsync(new List<Workflow> { entity }, user, cancellationToken);
+
+            entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(code, entity.Code);
+            Assert.AreEqual(string.Empty, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            var description = $"Description{DateTime.Now}";
+
+            entity.Description = description;
+            await repository.UpdateAsync(entity, user, cancellationToken);
+
+            entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
+            Assert.AreEqual(description, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            await repository.DeleteAsync(new List<string> { code }, user, cancellationToken);
 
             entity = await repository.GetByCodeAsync(code, new List<string>(), cancellationToken);
             Assert.IsNull(entity);
--- src/GenericWorkflowAPI.UnitTesting/TestRepositories/TestGenericRepository.cs
+++ src/GenericWorkflowAPI.UnitTesting/TestRepositories/TestGenericRepository.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GenericWorkflowAPI.Core.Extensions;
 using GenericWorkflowAPI.Database;
+using GenericWorkflowAPI.Domain;
 using GenericWorkflowAPI.Domain.Entities;
 using GenericWorkflowAPI.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,12 +15,107 @@ namespace GenericWorkflowAPI.UnitTesting
     public class TestGenericRepository : BaseTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestGetAllWithoutData()
         {
-            var dbContext = GetSqlServerDbContext(isInMemory: true);
-            var logger = GetLogger();
-            var entityService = new EntityService<Workflow>();
-            var repository = new GenericRepository<Workflow, ApplicationDbContext>(dbContext, logger, entityService);
+            ServicesExtensions.RegisterEncodingProvider();
+            var cancellationToken = new CancellationToken();
+            var repository = GetGenericCodeRepository<Workflow>(isInMemoryDbContext: true);
+
+            var emptyList = await repository.GetAllAsync(new List<string>(), cancellationToken);
+
+            Assert.IsNotNull(emptyList);
+            Assert.AreEqual(0, emptyList.Count);
+        }
+
+        [TestMethod]
+        public async Task TestGetByInvalidId()
+        {
+            ServicesExtensions.RegisterEncodingProvider();
+
+            var cancellationToken = new CancellationToken();
+            var repository = GetGenericCodeRepository<Workflow>(isInMemoryDbContext: true);
+
+            var entity = await repository.GetByIdAsync(-1, new List<string>(), cancellationToken);
+
+            Assert.IsNull(entity);
+        }
+
+        [TestMethod]
+        public async Task TestAddGetUpdateLoadedAndDelete()
+        {
+            ServicesExtensions.RegisterEncodingProvider();
+
+            var cancellationToken = new CancellationToken();
+            var repository = GetGenericCodeRepository<Workflow>(isInMemoryDbContext: true);
+
+            var user = new IdentityUser("admin") { Id = 1 };
+            var code = $"Test{DateTime.Now}";
+            var entity = new Workflow()
+            {
+                Code = code,
+                Description = string.Empty
+            };
+
+            await repository.AddRangeAsync(new List<Workflow> { entity }, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(code, entity.Code);
+            Assert.AreEqual(string.Empty, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            var description = $"Description{DateTime.Now}";
+
+            entity.Description = description;
+            await repository.UpdateLoadedAsync(entity, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.AreEqual(description, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            await repository.DeleteAsync(code, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.IsNull(entity);
+        }
+
+        [TestMethod]
+        public async Task TestAddRangeGetUpdateLoadedAndDelete()
+        {
+            ServicesExtensions.RegisterEncodingProvider();
+
+            var cancellationToken = new CancellationToken();
+            var repository = GetGenericCodeRepository<Workflow>(isInMemoryDbContext: true);
+
+            var user = new IdentityUser("admin") { Id = 1 };
+            var code = $"Test{DateTime.Now}";
+            var entity = new Workflow()
+            {
+                Code = code,
+                Description = string.Empty
+            };
+
+            await repository.AddRangeAsync(new List<Workflow> { entity }, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(code, entity.Code);
+            Assert.AreEqual(string.Empty, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            var description = $"Description{DateTime.Now}";
+
+            entity.Description = description;
+            await repository.UpdateLoadedAsync(entity, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.AreEqual(description, entity.Description);
+            Assert.IsFalse(entity.IsDeleted);
+
+            await repository.DeleteAsync(code, user, cancellationToken);
+
+            entity = await repository.GetByIdAsync(entity.Id, new List<string>(), cancellationToken);
+            Assert.IsNull(entity);
         }
     }
 }
